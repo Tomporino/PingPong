0c8cc7f7abb1a2a417881fe1c3a7fcc15f500e09
Tomporino <tomorpeter97@gmail.com>
2021-04-26 15:17:24 +0200

    Declare player rectangles, draw player one

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 import os
+from Player import Player
 
 FPS = 60
+
+VEL = 3
 
 WIDTH, HEIGHT = 900, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -11,14 +14,29 @@ TABLE = pygame.transform.scale(
             pygame.image.load(os.path.join("Assets", "pingpongtable.jpg"))
             , (WIDTH, HEIGHT))
 
-def draw_window():
+PLAYER_WIDTH, PLAYER_HEIGHT = 10, 50
+
+PLAYER_ONE = pygame.Rect(
+                            50, HEIGHT//2 + PLAYER_HEIGHT//2 #x y
+                            , PLAYER_WIDTH, PLAYER_HEIGHT
+                        )
+
+PLAYER_TWO = pygame.Rect(   
+                            WIDTH - 50, HEIGHT//2 + PLAYER_HEIGHT//2 #x, y
+                            , PLAYER_WIDTH, PLAYER_HEIGHT
+                            )
+
+def draw_window(player_one):
 
     WINDOW.blit(TABLE, (0,0))
+    pygame.draw.rect(WINDOW, BLACK, player_one)
 
     pygame.display.update()
 
 
 def main():
+
+    player_one = Player(PLAYER_ONE, VEL, {"Up": pygame.K_w, "Down": pygame.K_s})
 
     run = True
     clock = pygame.time.Clock()
@@ -31,7 +49,7 @@ def main():
                 run = False
                 pygame.quit()
 
-        draw_window()
+        draw_window(player_one.player)
 
 
 
